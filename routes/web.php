20ec17760bd19cf9b27588b129893c00193b36b8
unknown <dev.ismael305@gmail.com>
2022-03-27 15:07:19 +0200

    add navbar to laravel project

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Home                           
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\Web\HomeController::class, 'index'])->name('home');
+                                                                                                                                       

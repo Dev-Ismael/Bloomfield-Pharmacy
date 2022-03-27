afd8daf954694d198e8582183bdd85c4a2d30af0
unknown <dev.ismael305@gmail.com>
2022-03-27 10:46:25 +0200

    add laravel 8 with auth

--- sites/all/themes/custom/fp/offer-fillter.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $( ".more-less-btn" ).click(function() {
-      $( '.fillter-links' ).toggleClass( "height-normal");
-      $(".less").toggle();
-      $(".more").toggle();
-    });
-  });
--- sites/all/themes/custom/fp/taxonomy.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    $(document).ready(function() { 
-
-    $('.fillter-toggle').click(function() {
-    $( '.fillter-main' ).toggleClass('open-fillter');
-    $( this ).toggleClass('colse-btn');
-    });
-    });
-}(jQuery));

6f8873378d90f9eac291af3d0e3395e428413766
unknown <dev.ismael305@gmail.com>
2022-04-13 13:32:22 +0200

    Add some style at profile page

--- resources/views/web/profile.blade.php
+++ resources/views/web/profile.blade.php
@@ -22,57 +22,59 @@
                                 <div class="d-flex flex-row align-items-center back"><i class="fa fa-long-arrow-left mr-1 mb-1"></i>
                                     <a href="{{ route("home") }}" class="text-dark"><h4>Back to home</h4></a>
                                 </div>
-                                <h4 class="text-right">Edit Profile</h4>
+                                <h4 class="text-right"> <i class="fa-solid fa-pen-to-square"></i> Edit Profile</h4>
                             </div>
-                            <div class="row">
-                                <div class="col-md-6 form-group text-left">
-                                    <label for="firstName" class="text-black">First Name</label>
-                                    <input type="text" class="form-control" id="firstName"  placeholder="Type First Name...">
+                            <form action="#">
+                                <div class="row">
+                                    <div class="col-md-6 form-group text-left">
+                                        <label for="firstName" class="text-black"> <i class="fa-solid fa-file-signature"></i> First Name</label>
+                                        <input type="text" class="form-control" id="firstName"  placeholder="Type First Name...">
+                                    </div>
+                                    <div class="col-md-6 form-group text-left">
+                                        <label for="lastName" class="text-black"> <i class="fa-solid fa-file-signature"></i> Last Name</label>
+                                        <input type="text" class="form-control" id="lastName" placeholder="Type Last Name...">
+                                    </div>
                                 </div>
-                                <div class="col-md-6 form-group text-left">
-                                    <label for="lastName" class="text-black">Last Name</label>
-                                    <input type="text" class="form-control" id="lastName" placeholder="Type Last Name...">
+                                <div class="row">
+                                    <div class="col-md-6 form-group text-left">
+                                        <label for="lastName" class="text-black"> <i class="fa-solid fa-phone-volume"></i> Phone </label>
+                                        <input type="text" class="form-control" id="lastName" placeholder="Type Phone Number...">
+                                    </div>
+                                    <div class="col-md-6 form-group text-left">
+                                        <label for="lastName" class="text-black"> <i class="fa-solid fa-phone-volume"></i> Second Phone (optional) </label>
+                                        <input type="text" class="form-control" id="lastName" placeholder="Type Phone Number...">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 form-group text-left">
-                                    <label for="lastName" class="text-black">Phone </label>
-                                    <input type="text" class="form-control" id="lastName" placeholder="Type Phone Number...">
+                                <div class="row">
+                                    <div class="col-md-6 form-group text-left">
+                                        <label for="email2" class="text-black"> <i class="fa-solid fa-envelope"></i> Second Email (optional) </label>
+                                        <input type="text" class="form-control" id="email2"  placeholder="Type Email Address...">
+                                    </div>
+                                    <div class="col-md-6 form-group text-left">
+                                        <label for="state" class="text-black"> <i class="fa-solid fa-flag-usa"></i> State/Region </label>
+                                        <input type="text" class="form-control" id="state"  placeholder="Type State/Region...">
+                                    </div>
                                 </div>
-                                <div class="col-md-6 form-group text-left">
-                                    <label for="lastName" class="text-black"> Second Phone (optional) </label>
-                                    <input type="text" class="form-control" id="lastName" placeholder="Type Phone Number...">
+                                <div class="row">
+                                    <div class="col-md-12 form-group text-left">
+                                        <label for="address" class="text-black"> <i class="fa-solid fa-location-dot"></i> Address </label>
+                                        <input type="text" class="form-control" id="address" placeholder="Type Address Details...">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 form-group text-left">
-                                    <label for="email2" class="text-black">Second Email (optional) </label>
-                                    <input type="text" class="form-control" id="email2"  placeholder="Type Email Address...">
+                                <div class="row">
+                                    <div class="col-md-12 form-group text-left">
+                                        <label for="address2" class="text-black"> <i class="fa-solid fa-location-dot"></i> Second Address (optional) </label>
+                                        <input type="text" class="form-control" id="address2" placeholder="Type Address Details...">
+                                    </div>
                                 </div>
-                                <div class="col-md-6 form-group text-left">
-                                    <label for="state" class="text-black"> State/Region </label>
-                                    <input type="text" class="form-control" id="state"  placeholder="Type State/Region...">
+                                <div class="row">
+                                    <div class="col-md-12 form-group text-left">
+                                        <label for="address3" class="text-black"> <i class="fa-solid fa-location-dot"></i> Third Address (optional) </label>
+                                        <input type="text" class="form-control" id="address3" placeholder="Type Address Details...">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 form-group text-left">
-                                    <label for="address" class="text-black">Address </label>
-                                    <input type="text" class="form-control" id="address" placeholder="Type Address Details...">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 form-group text-left">
-                                    <label for="address2" class="text-black"> Second Address (optional) </label>
-                                    <input type="text" class="form-control" id="address2" placeholder="Type Address Details...">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 form-group text-left">
-                                    <label for="address3" class="text-black"> Third Address (optional) </label>
-                                    <input type="text" class="form-control" id="address3" placeholder="Type Address Details...">
-                                </div>
-                            </div>
-                            <div class="mt-5 text-right"><button class="btn btn-primary profile-button" type="button"> <i class="fa-solid fa-floppy-disk"></i> Save Profile</button></div>
+                                <div class="mt-5 text-right"><button class="btn btn-primary profile-button" type="button"> <i class="fa-solid fa-floppy-disk"></i> Save Profile</button></div>
+                            </form>
                         </div>
                     </div>
                 </div>

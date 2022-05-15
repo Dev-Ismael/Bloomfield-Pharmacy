54b0a90aa146262ffaffb5d0162eb1c896f2ace1
unknown <dev.ismael305@gmail.com>
2022-05-15 14:36:41 +0200

    create seeder/factory admin/category

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+use Cviebrock\EloquentSluggable\Services\SlugService;
+
+class CategoryFactory extends Factory
+{
+
+        /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->catchPhrase,
+            'slug'  => $this->faker->catchPhrase,
+            'img'  =>  'category.png' ,
+        ];
+    }
+}

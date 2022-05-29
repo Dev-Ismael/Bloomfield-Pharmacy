a2f8d588ea5f01bbd7a0d2ac1202ff01dba158d6
unknown <dev.ismael305@gmail.com>
2022-05-29 11:45:56 +0200

    ad product item in pages

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -170,23 +170,52 @@
                             @foreach ( $lastedOffers as $product )
                                 <div class="col-xs-12">
                                     <div class="col-xs-12 remove-padding product-item">
-                                        <a href="{{ route("product", $product->slug ) }}" class="item-img" tabindex="0">
-                                            <img src="{{ asset('images/products/'.$product->img) }}" width="220" height="220"
-                                                alt="">
-                                            <span class="off-span">UP TO {{ $product->offer_percentage }} %</span>
+                                        <a href="{{ route('product',$product->slug) }}" class="item-img" tabindex="0">
+                                            <img  
+                                                src="{{ asset('images/products/'.$product->img) }}"
+                                                width="220" height="220" alt="">
+                                            @if( $product->has_offer == '1')
+                                                <span class="off-span">UP TO {{ $product->offer_percentage }} %</span>
+                                            @endif
                                         </a>
                                         <div class="product-txt-container"> <p> {{ $product->brand }} </p></div>
                                         <div class="product-txt-container"><a href="{{ route("product", $product->slug ) }}" tabindex="0">  {{ $product->title }}  </a></div>
                                         <div class="product-txt-container"> <p> {{ $product->measurement }} </p></div>
                                         <div class="price">
-                                            {{ $product->final_price }}$ 
-                                            <span class="uc-price"> {{ $product->price }}$</span> 
+                                            {{ $product->final_price }}$
+                                            
+                                            @if( $product->has_offer == '1')
+                                                <span class="uc-price">
+                                                    {{ $product->price }}$
+                                                </span> 
+                                            @endif
                                         </div>
-                                        <div class="col-xs-12 add-cart-main text-center">
-                                            <button href="#" product_id="{{ $product->id }}"> <i class="fa-solid fa-cart-shopping"></i> Cart </button>
-                                            &nbsp;
-                                            &nbsp;
-                                            <button href="#" product_id="{{ $product->id }}"> <i class="fa-solid fa-heart"></i> Wishlist </button>
+                                        <div class="col-xs-12 product-buttons d-flex justify-content-center align-content-center text-center">
+                                            @auth
+                                                @php
+                                                    $wishlist = App\Models\Wishlist::where("user_id", Auth::id())->where("product_id", $product->id)->first();
+                                                    $cart     = App\Models\Cart::where("user_id", Auth::id())->where("product_id", $product->id)->first();
+                                                @endphp
+                                                <div class="cart">
+                                                    @if ( $cart != Null )
+                                                        <p> <i class="fa-solid fa-check"></i> Cart  </p>
+                                                    @else
+                                                        <button href="#" class="add-cart" product_id="{{ $product->id }}"> <i class="fa-solid fa-cart-shopping"></i> Cart </button>
+                                                    @endif
+                                                </div>
+
+                                                <div class="wishlist">
+                                                    @if ( $wishlist != Null )
+                                                        <p> <i class="fa-solid fa-check"></i> wishlist  </p>
+                                                    @else
+                                                        <button href="#" class="add-wishlist" product_id="{{ $product->id }}"> <i class="fa-solid fa-heart"></i> wishlist </button>
+                                                    @endif
+                                                </div>
+                                            <!------ if guest -------->
+                                            @else
+                                                <button href="#" data-toggle="modal" data-target="#login"> <i class="fa-solid fa-cart-shopping"></i> Cart </button>
+                                                <button href="#" data-toggle="modal" data-target="#login" > <i class="fa-solid fa-heart"></i> Wishlist </button>
+                                            @endauth
                                         </div>
                                     </div>
                                 </div>

06cc05ebc9ba957eb635df3e607c2d3e346ae2a0
Abdulrahman Ismael <64872640+Dev-Ismael@users.noreply.github.com>
2022-06-05 00:44:02 +0200

    add stats card to dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Messege;
+use App\Models\Order;
+use App\Models\Prescription;
+use App\Models\Product;
+use App\Models\Subcategory;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
 
     public function index()
     {
-        return view('admin.dashboard');
+        $users = User::count();
+        $categories = Category::count();
+        $subcategories = Subcategory::count();
+        $products = Product::count();
+        $orders = Order::count();
+        $prescriptions = Prescription::count();
+        $messeges = Messege::count();
+
+        return view('admin.dashboard',
+            compact('users', 'categories', 'subcategories',
+            'products', 'orders', 'prescriptions', 'messeges' ));
     }
 
 }
